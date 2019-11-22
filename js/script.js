71c37995b17e682934c19d568247ed9822759590
meurin53 <thomas.meurin@edu.ece.fr>
2019-11-22 10:55:52 +0100

    rendu TP3

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 
 // Fonction appelée lors du click du bouton
 function start() {
+
+   
+  city = document.getElementById('city-input').value; //q3
+  if(city === "")
+  { city=undefined;
+  }
+
   // Création de l'objet apiWeather
-  const apiWeather = new API_WEATHER();
+  const apiWeather = new API_WEATHER(city);
   // Appel de la fonction fetchTodayForecast
 
   apiWeather
@@ -28,4 +35,30 @@ function start() {
       // Affiche une erreur
       console.error(error);
     });
+
+//recup des 3 jours
+
+    apiWeather
+    .fetchTroisjourForecast()
+    .then(function(response) {
+      // Récupère la donnée d'une API
+      const data2 = response.data;
+      let jours=0;
+
+
+      data2.list.forEach((data2, index)=> {
+      jours++;
+
+      document.getElementById(`${jours}-forecast-main`).innerHTML = data2.weather[0].main;
+      document.getElementById(`${jours}-forecast-more-info`).innerHTML = data2.weather[0].description;
+      document.getElementById(`${jours}-icon-weather-container`).innerHTML = apiWeather.getHTMLElementFromIcon(data2.weather[0].icon);
+      document.getElementById(`${jours}-forecast-temp`).innerHTML = `${data2.temp.day}°C`;
+      
+    });
+
+  
+  });
+  console.log(apiWeather.city);
+
 }
+
